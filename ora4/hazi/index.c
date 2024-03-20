aa666f892aa51ca1e08376a1646dbc3a3d6ba3d0
Gsbi18 <gabi.ancsan@gmail.com>
2024-03-20 10:20:13 +0100

    Házi megnézve

--- ora4/hazi/index.c
+++ ora4/hazi/index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int contains(int n, int tomb[], int szam)
+int index(int n, int tomb[], int szam)
 {
     for (int i = 0; i < n; i++)
     {
@@ -9,7 +9,7 @@ int contains(int n, int tomb[], int szam)
             return i;
         }
     }
-    return 0;
+    return -1;
 }
 
 int main()
@@ -20,9 +20,9 @@ int main()
 
     int szamok[10] = {0, 1, 2, 3, 5, 6, 7, 8, 9,4};
     int szamok_meret = 10;
-    if (contains(szamok_meret, szamok, kereset) != 0)
+    if (index(szamok_meret, szamok, kereset) > -1)
     {
-        printf("A %d benne van a tömbben a %d helyen.\n", kereset,contains(szamok_meret, szamok, kereset));
+        printf("A %d benne van a tömbben a %d helyen.\n", kereset,index(szamok_meret, szamok, kereset));
     }
     else
     {
